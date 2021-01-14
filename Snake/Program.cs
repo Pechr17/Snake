2fd0dc99a8855dd53af39ce37713e73b8342fe61
pahc9 <pahc9@LAPTOP-UG979AH0>
2021-01-14 12:06:23 +0100

    create and print board with random apple

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Snake
 {
-    class Body
+    class board
     {
-        static void Main(string[] args)
+        public int rows, columns;
+        public List<List<char>> game; //the board
+        private static Random rnd = new Random(); //used to generate apple position
+      
+        private bool appleIsEaten = true;
+        public board(int r, int c)
         {
-            Console.WriteLine("Hello World!");
+            rows = r;
+            columns = c;
+        }
+
+        public void initGame()
+        {
+            game = new List<List<char>>();
+            for(int i = 0; i < rows + 2 ; i++)
+            {
+                game.Add(new List<char>());
+                for(int j = 0; j < columns + 2; j++)
+                {
+                    if (i == 0 || i == rows + 1 || j == 0 || j == rows + 1)
+                    {
+                        game[i].Add('X'); // creates border
+                    }
+                    else
+                    {
+                        game[i].Add(' ');
+                    }
+                }
+            }
+        }
+        void ChangeapplePos()
+        {
+            game[rnd.Next(1, rows)][rnd.Next(1, columns)] = 'O'; // Apple 
+        }
+        public void printBoard()
+        {
+            if (appleIsEaten)
+            {
+                ChangeapplePos();
+            }
+            for (int i = 0; i < rows+2; i++)
+            {
+                for(int j = 0; j < columns+2; j++)
+                {
+                    Console.Write(game[i][j]);
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+        public void updateBoard()
+        {
+
         }
     }
 }
---
+++ Snake/Source.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Source
+    {
+        static void Main(string[] args)
+        {
+            board t1 = new board(5, 5);
+            t1.initGame();
+            t1.printBoard();
+        }
+    }
+}
